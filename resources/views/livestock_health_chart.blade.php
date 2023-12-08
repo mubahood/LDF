a0c53c4bd4f8d6ad33b9a48d542cc2b8b0905b19
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2023-12-08 12:35:29 +0300

    forms

--- resources/views/livestock_health_chart.blade.php
+++ resources/views/livestock_health_chart.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +9,22 @@
 </head>
 <body>
     <div style="width: 80%; margin: auto;">
+        <label for="dateFilter">Filter by Date:</label>
+        <select id="dateFilter" onchange="updateChart()">
+            <option value="all">All Dates</option>
+            @foreach ($timestamps as $timestamp)
+                <option value="{{ $timestamp }}">{{ $timestamp }}</option>
+            @endforeach
+        </select>
+
+        <label for="statusFilter">Filter by Status:</label>
+        <select id="statusFilter" onchange="updateChart()">
+            <option value="all">All Statuses</option>
+            @foreach ($datasets as $dataset)
+        <option value="{{ $dataset['label'] }}">{{ $dataset['label'] }}</option>
+    @endforeach
+        </select>
+
         <canvas id="healthChart"></canvas>
     </div>
 
@@ -17,40 +32,72 @@
         // Get data from the PHP variables passed to the view
         var timestamps = <?php echo json_encode($timestamps); ?>;
         var datasets = <?php echo json_encode($datasets); ?>;
-  
-        // Create the line chart
-  var ctx = document.getElementById('healthChart').getContext('2d');
-        var healthChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: timestamps,
-                datasets: datasets,
+
+   // Create the initial line chart
+   var ctx = document.getElementById('healthChart').getContext('2d');
+    var healthChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timestamps,
+            datasets: datasets,
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            title: {
+                display: true,
+                text: 'LiveStock Health Chart',
             },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                title: {
-                    display: true,
-                    text: 'LiveStock Health Chart',
-                },
-                scales: {
-                    x: {
-                        type: 'category',
-                        labels: timestamps,
-                        title: {
-                            display: true,
-                            text: 'Date',
-                        },
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: 'Count',
-                        },
+            scales: {
+                x: {
+                    type: 'category',
+                    labels: timestamps,
+                    title: {
+                        display: true,
+                        text: 'Date',
                     },
                 },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Count',
+                    },
+                },
             },
-        });
+        },
+    });
+// Function to update the chart based on the selected filters
+// Function to update the chart based on the selected filters
+function updateChart() {
+    var dateFilter = document.getElementById('dateFilter').value;
+    var statusFilter = document.getElementById('statusFilter').value;
+
+    // Filter timestamps
+    var filteredTimestamps = timestamps.filter(function (timestamp, index) {
+        return dateFilter === 'all' || timestamp === dateFilter;
+    });
+
+    // Filter datasets based on the selected options
+    var filteredDatasets = datasets.map(function (dataset) {
+        return {
+            label: dataset.label,
+            data: dataset.data.filter(function (_, index) {
+                return (dateFilter === 'all' || timestamps[index] === dateFilter) &&
+                    (statusFilter === 'all' || dataset.label === statusFilter);
+            }),
+            fill: false,
+        };
+    });
+
+    // Update the existing chart with the filtered data and options
+    healthChart.data.labels = filteredTimestamps;
+    healthChart.data.datasets = filteredDatasets;
+    healthChart.options.scales.x.labels = filteredTimestamps;
+    healthChart.update();
+}
+
+
+
     </script>
 </body>
 </html>
